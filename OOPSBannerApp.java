2b44a389b1d48c50691f14220e9a91b029acbd18
uy3854-collab <uy3854@srmist.edu.in>
2026-02-17 11:08:56 +0530

    UC3 completed - Banner using String.join()

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,21 +1,42 @@
 /**
- * UC2 - Print OOPS Banner
- * Displays OOPS in large ASCII banner format.
+ * UC3 - OOPS Banner using String.join()
+ * Improves memory efficiency over string concatenation.
  * 
  * @author Umesh
- * @version 2.0
+ * @version 3.0
  */
 
 public class OOPSBannerApp {
 
     public static void main(String[] args) {
 
-        System.out.println(" *****   *****   *****   ***** ");
-        System.out.println("*     * *     * *     * *     *");
-        System.out.println("*     * *     * *     * *     *");
-        System.out.println("*     * *     * *****   ***** ");
-        System.out.println("*     * *     * *           * ");
-        System.out.println("*     * *     * *           * ");
-        System.out.println(" *****   *****  *      *****  ");
+        String line1 = String.join(" ",
+                " ***** ", " ***** ", " ***** ", " ***** ");
+
+        String line2 = String.join(" ",
+                "*     *", "*     *", "*     *", "*     *");
+
+        String line3 = String.join(" ",
+                "*     *", "*     *", "*     *", "*     *");
+
+        String line4 = String.join(" ",
+                "*     *", "*     *", " ***** ", " ***** ");
+
+        String line5 = String.join(" ",
+                "*     *", "*     *", "*      ", "      *");
+
+        String line6 = String.join(" ",
+                "*     *", "*     *", "*      ", "      *");
+
+        String line7 = String.join(" ",
+                " ***** ", " ***** ", "*      ", " ***** ");
+
+        System.out.println(line1);
+        System.out.println(line2);
+        System.out.println(line3);
+        System.out.println(line4);
+        System.out.println(line5);
+        System.out.println(line6);
+        System.out.println(line7);
     }
 }
