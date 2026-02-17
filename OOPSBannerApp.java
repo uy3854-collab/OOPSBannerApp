ee9eaec02fc80591e6d9ccc71e921c5b6e861173
uy3854-collab <uy3854@srmist.edu.in>
2026-02-17 10:35:12 +0530

    UC1 completed - Print OOPS

---
+++ OOPSBannerApp.java
@@ -0,0 +1,14 @@
+/**
+ * UC1 - Print OOPS Banner
+ * This program prints the text "OOPS" to the console.
+ * 
+ * @author Umesh
+ * @version 1.0
+ */
+
+public class OOPSBannerApp {
+
+    public static void main(String[] args) {
+        System.out.println("OOPS");
+    }
+}
