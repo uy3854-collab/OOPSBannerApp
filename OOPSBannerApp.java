391105cca520cd79a9e9086f8a0ca07235652e97
uy3854-collab <uy3854@srmist.edu.in>
2026-03-11 18:26:49 +0530

    UC7 completed - CharacterPattern class implementation

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,55 +1,64 @@
 public class OOPSBannerApp {
 
-    public static void main(String[] args) {
+    static class CharacterPattern
+    {
+        char character;
+        String pattern[];
 
-        String O[] = printO();
-        String P[] = printP();
-        String S[] = printS();
+        CharacterPattern(char character, String pattern[])
+        {
+            this.character = character;
+            this.pattern = pattern;
+        }
+
+        public String[] getPattern()
+        {
+            return pattern;
+        }
+    }
+
+    public static void main(String[] args)
+    {
+
+        CharacterPattern O = new CharacterPattern('O', new String[]{
+                " ***** ",
+                "*     *",
+                "*     *",
+                "*     *",
+                "*     *",
+                "*     *",
+                " ***** "
+        });
+
+        CharacterPattern P = new CharacterPattern('P', new String[]{
+                "****** ",
+                "*     *",
+                "*     *",
+                "****** ",
+                "*      ",
+                "*      ",
+                "*      "
+        });
+
+        CharacterPattern S = new CharacterPattern('S', new String[]{
+                " ***** ",
+                "*     *",
+                "*      ",
+                " ***** ",
+                "      *",
+                "*     *",
+                " ***** "
+        });
+
+        String oPattern[] = O.getPattern();
+        String pPattern[] = P.getPattern();
+        String sPattern[] = S.getPattern();
 
         for(int i = 0; i < 7; i++)
         {
-            System.out.println(O[i] + "  " + O[i] + "  " + P[i] + "  " + S[i]);
+            System.out.println(oPattern[i] + "  " + oPattern[i] + "  " + pPattern[i] + "  " + sPattern[i]);
         }
 
-    }
-
-    public static String[] printO()
-    {
-        return new String[] {
-            " ***** ",
-            "*     *",
-            "*     *",
-            "*     *",
-            "*     *",
-            "*     *",
-            " ***** "
-        };
-    }
-
-    public static String[] printP()
-    {
-        return new String[] {
-            "****** ",
-            "*     *",
-            "*     *",
-            "****** ",
-            "*      ",
-            "*      ",
-            "*      "
-        };
-    }
-
-    public static String[] printS()
-    {
-        return new String[] {
-            " ***** ",
-            "*     *",
-            "*      ",
-            " ***** ",
-            "      *",
-            "*     *",
-            " ***** "
-        };
     }
 
 }
